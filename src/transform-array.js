1bb8d8db3b5f5ba70904a4c671cc4e5dc3e547d5
Viktar Svintsitski <jetalai3@gmail.com>
2020-09-13 15:26:27 +0300

    feat: complete 65 out of 70 test cases

--- src/transform-array.js
+++ src/transform-array.js
@@ -3,16 +3,22 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function transform(arr) {
   if (!Array.isArray(arr)) throw new Error;
   let resultArr = [];
-
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === '--discard-next' && i < arr.length - 1) {
-      i+=1;
-    } else  if (i > 0 && arr[i] === '--discard-prev' ) {
-      resultArr.pop();
-    } else if (arr[i] === '--double-next' && i < arr.length - 1) {
-      resultArr.push(arr[i + 1], arr[i + 1]);
-    } else if (i > 0 && arr[i] === '--double-prev') {
-      resultArr.push(arr[i - 1]);
+    if (arr[i] === '--discard-next') {
+      if (arr[i + 2] === '--double-prev' || arr[i + 2] === '--discard-prev') i++;
+      i++;
+    } else if (arr[i] === '--discard-prev') {
+      if (i !== 0) {
+        resultArr.pop();
+      }
+    } else if (arr[i] === '--double-next') {
+      if (i < arr.length - 1) {
+        resultArr.push(arr[i + 1]);
+      }
+    } else if (arr[i] === '--double-prev') {
+      if (i !== 0) {
+        resultArr.push(arr[i - 1]);
+      }
     } else {
       resultArr.push(arr[i]);
     }
